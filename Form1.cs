1c8f4fe9e132b4d8078c4f40fde3df16b9045dda
tungyao <1922527051@qq.com>
2021-11-16 22:28:35 +0800

    update

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
 
 namespace WindowsFormsApp1
 {
@@ -15,6 +16,26 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+            var url = new System.Uri("file://C:\\Users\\yao19\\RiderProjects\\WindowsFormsApp1\\web\\index.html");
+            this.webView21.Source = url;
+            this.InitializeAsync();
+        }
+        async void InitializeAsync()
+        {
+            await webView21.EnsureCoreWebView2Async(null);
+            webView21.CoreWebView2.WebMessageReceived += MessageReceived;
+            this.Post();
+
+        }
+
+        private void MessageReceived(object sender, CoreWebView2WebMessageReceivedEventArgs e)
+        {
+            
+        }
+
+        private void Post()
+        {
+            this.webView21.CoreWebView2.PostWebMessageAsString("{\"hello\":\"world\"}");
         }
     }
 }
